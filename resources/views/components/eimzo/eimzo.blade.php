d592ab18d216de7714a57368478e5a6276b9e5e6
arabic0513 <rustamyusupov230@gmail.com>
2023-01-29 16:34:05 +0500

    eimzo button color

--- resources/views/components/eimzo/eimzo.blade.php
+++ resources/views/components/eimzo/eimzo.blade.php
@@ -62,9 +62,10 @@
     <div class="d-flex justify-content-evenly m-4">
         <button type="button"
                 style="width: 15em;"
-                class="btn btn-success"
+                class="btn btn-primary"
                 id="eri_sign" onclick="sign()">
             <i class="fa fa-credit-card"></i>
+            &nbsp;
             Вход или Регистрация
         </button>
     </div>
