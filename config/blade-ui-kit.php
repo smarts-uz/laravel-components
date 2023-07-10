79e0d7480b1a71136e661377fcf8d5ce2fdce1e8
arabic0513 <rustamyusupov230@gmail.com>
2023-07-10 13:29:04 +0500

    deleted not used cdn links

--- config/blade-ui-kit.php
+++ config/blade-ui-kit.php
@@ -66,43 +66,6 @@ return [
     */
 
     'assets' => [
-
-        'alpine' => 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.3.5/dist/alpine.min.js',
-
-        'easy-mde' => [
-            'https://unpkg.com/easymde/dist/easymde.min.css',
-            'https://unpkg.com/easymde/dist/easymde.min.js',
-        ],
-
-        'flat-pickr' => [
-            'https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css',
-            'https://cdn.jsdelivr.net/npm/flatpickr@4.6.9/dist/flatpickr.min.js',
-        ],
-
-        'mapbox' => [
-            'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css',
-            'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.js',
-        ],
-
-        'moment' => [
-            'https://cdn.jsdelivr.net/npm/moment@2.26.0/moment.min.js',
-            'https://cdn.jsdelivr.net/npm/moment-timezone@0.5.31/builds/moment-timezone-with-data.min.js',
-        ],
-
-        'pickr' => [
-            'https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/themes/classic.min.css',
-            'https://cdn.jsdelivr.net/npm/@simonwep/pickr/dist/pickr.min.js',
-        ],
-
-        'pikaday' => [
-            'https://cdn.jsdelivr.net/npm/pikaday/css/pikaday.css',
-            'https://cdn.jsdelivr.net/npm/pikaday/pikaday.js',
-        ],
-
-        'trix' => [
-            'https://unpkg.com/trix@1.2.3/dist/trix.css',
-            'https://unpkg.com/trix@1.2.3/dist/trix.js',
-        ],
         'eimzo_login' => [
             "vendor/eimzo/assets/js/eimzo/e-imzo.js",
             "vendor/eimzo/assets/js/eimzo/e-imzo-client.js",
